b595f1a937892e716d79e1f1cf700a92df1440a3
jhlee <wisdom_13_@naver.com>
2024-03-23 17:07:53 +0900

    🔧 위젯 - 프로필형

---
+++ app/(house)/(routes)/[houseAddress]/_components/WidgetProfile.tsx
@@ -0,0 +1,45 @@
+'use client';
+
+import { Avatar, AvatarImage, AvatarFallback } from '@/components/ui/avatar';
+import { Button } from '@/components/ui/button';
+import useHouseBuild from '@/hooks/useHouseBuild';
+import { cn } from '@/lib/utils';
+import { Widget } from '@/types';
+
+interface WidgetProfileProps {
+  widget: Widget;
+}
+const WidgetProfile = ({
+  widget
+}: WidgetProfileProps) => {
+  const { houseBuild } = useHouseBuild();
+  const owner = houseBuild?.family.filter((item) => item.is_owner)[0];
+
+  const horizontal = widget.grid.col > widget.grid.row;
+
+  return (
+    <div className={cn(
+      'flex items-center justify-center gap-4 h-full p-4 ',
+      !horizontal && 'flex-col text-center'
+    )}>
+      <Avatar
+        className={cn(
+          'w-24 h-24 border border-black',
+          horizontal ? 'w-20 h-20' : 'w-24 h-24'
+        )}
+      >
+        <AvatarImage src={owner?.avatar_url} />
+        <AvatarFallback className='text-xs'>{owner?.nick_name?.charAt(0)}</AvatarFallback>
+      </Avatar>
+      <div className='flex flex-col'>
+        <h2 className='text-xl font-semibold'>{owner?.nick_name}</h2>
+        <p className={cn(
+          'text-sm',
+          horizontal ? 'line-clamp-2' : 'line-clamp-3'
+        )}>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Exercitationem maiores iste fugit hic nesciunt! Alias rem doloremque, est sit voluptatem consequatur accusamus ab repudiandae nostrum exercitationem? Ipsa magnam quia culpa.</p>
+      </div>
+    </div>
+  );
+}
+
+export default WidgetProfile;
