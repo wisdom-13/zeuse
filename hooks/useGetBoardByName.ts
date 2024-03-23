cfacf5adff5c4419543ff65bf599909b0a33f953
jhlee <wisdom_13_@naver.com>
2024-03-23 17:08:29 +0900

    🔧 게시판 포스트 리스트 호출 hook

---
+++ hooks/useGetBoardByName.ts
@@ -0,0 +1,46 @@
+import { BoardList } from '@/types';
+import { useSessionContext } from '@supabase/auth-helpers-react';
+import { useEffect, useMemo, useState } from 'react'
+import { toast } from 'sonner';
+
+const useGetBoardByName = (board_id?: string) => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [board, setBoard] = useState<BoardList | undefined>(undefined);
+  const { supabaseClient } = useSessionContext();
+
+  useEffect(() => {
+    if (!board_id) {
+      return;
+    }
+
+    setIsLoading(true);
+
+    const fetchBoard = async () => {
+      const { data, error } = await supabaseClient
+        .from('board')
+        .select(`
+        *,
+        posts:posts!left(*, family:family_id (*))
+      `)
+        .eq('id', board_id)
+        .single();
+
+      if (error) {
+        setIsLoading(false);
+        return toast.error(error.message);
+      }
+
+      setBoard(data as BoardList);
+      setIsLoading(false);
+    }
+
+    fetchBoard();
+  }, [board_id, supabaseClient]);
+
+  return useMemo(() => ({
+    isLoading,
+    board
+  }), [isLoading, board]);
+}
+
+export default useGetBoardByName;
