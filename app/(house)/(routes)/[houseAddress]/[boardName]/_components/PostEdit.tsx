8337fb5f7f10a9f6f2afc18b1c02f999bc72816f
jhlee <wisdom_13_@naver.com>
2024-04-09 23:52:49 +0900

    🔧 포스트 상세 옵션 설정

--- app/(house)/(routes)/[houseAddress]/[boardName]/_components/PostEdit.tsx
+++ app/(house)/(routes)/[houseAddress]/[boardName]/_components/PostEdit.tsx
@@ -10,8 +10,26 @@ import { toast } from 'sonner';
 import { useSupabaseClient } from '@supabase/auth-helpers-react';
 import { useParams, useRouter } from 'next/navigation';
 import useGetBoardByName from '@/hooks/useGetBoardByName';
+import Image from 'next/image';
+import { getPublicUrl } from '@/util/getPublicUrl';
+import { Globe, ImageIcon, Lock } from 'lucide-react';
+import { Separator } from '@/components/ui/separator';
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog"
 
-const PostEdit = () => {
+interface PostEditProps {
+  familyId: string;
+}
+
+const PostEdit = ({
+  familyId
+}: PostEditProps) => {
   const param = useParams();
   const router = useRouter();
   const Editor = useMemo(() => dynamic(() => import("../../_components/Editor"), { ssr: false }), []);
@@ -19,6 +37,8 @@ const PostEdit = () => {
   const { board } = useGetBoardByName(param.houseAddress, param.boardName);
   const [title, setTitle] = useState('');
   const [content, setContent] = useState('');
+  const [thumbnailPath, setThumbnailPath] = useState('');
+  const [isSetting, setIsSetting] = useState(false);
 
   if (!board) {
     return;
@@ -38,10 +58,12 @@ const PostEdit = () => {
     const { data, error } = await supabaseClient
       .from('posts')
       .insert({
+        family_id: familyId,
         board_id: board.id,
         title: title,
         content: content,
-        family_id: '36fee6c4-35c6-464e-a3d0-f08bd0fbfb3d'
+        thumbnailPath: thumbnailPath,
+        created_at: new Date(),
       })
       .select()
       .single();
@@ -59,27 +81,86 @@ const PostEdit = () => {
   return (
     <>
       <ScrollArea className='w-full h-full pt-6'>
-        <div className='flex flex-col gap-y-4'>
-          <div className='flex items-center px-6 mt-10'>
-            <Input
-              value={title}
-              onChange={(e) => setTitle(e.target.value)}
-              className='text-4xl font-semibold border-none px-0'
-              placeholder='제목을 입력하세요'
-            />
+        {!isSetting ? (
+          <div className='flex flex-col gap-y-4'>
+            <div className='flex items-center px-6 mt-10'>
+              <Input
+                value={title}
+                onChange={(e) => setTitle(e.target.value)}
+                className='text-4xl font-bold border-none px-0'
+                placeholder='제목을 입력하세요'
+              />
+            </div>
+            <div className='flex flex-col'>
+              <Editor
+                editable={true}
+                onChange={setContent}
+                setThumbnailPath={setThumbnailPath}
+                thumbnailPath={thumbnailPath}
+              />
+            </div>
           </div>
-          <div className='flex flex-col'>
-            <Editor
-              onChange={setContent}
-              editable={true}
-            />
+        ) : (
+          <div className='flex flex-col p-6'>
+            <div className='flex flex-col gap-y-4'>
+              <h3 className='text-2xl font-bold'>포스트 미리보기</h3>
+              <div className='w-60 h-36 bg-gray-200 flex flex-col items-center justify-center rounded-md'>
+                {
+                  thumbnailPath ? (
+                    <div className='w-44 h-10 relative'>
+                      <Image
+                        src={getPublicUrl(`post/${thumbnailPath}`)}
+                        alt='thumbnail'
+                        fill
+                        className='object-cover'
+                      />
+                    </div>
+                  ) : (
+                    <>
+                      <ImageIcon size={36} />
+                      <span className='text-sm mt-2'>썸네일 업로드</span>
+                    </>
+                  )
+                }
+              </div>
+            </div>
+            <Separator orientation='vertical' className='mx-10' />
+            <div className='flex flex-col gap-y-4'>
+              <h3 className='text-2xl font-bold'>공개설정</h3>
+              <div className='flex gap-x-2'>
+                <Button variant='outline' size='lg' className='flex gap-x-2 font-semibold w-36'>
+                  <Globe size={18} />
+                  전체 공개
+                </Button>
+                <Button variant='secondary' size='lg' className='flex gap-x-2 font-semibold w-36'>
+                  <Lock size={18} />
+                  비공개
+                </Button>
+              </div>
+            </div>
           </div>
-        </div>
+        )}
+
       </ScrollArea>
       <div className='flex gap-x-2 p-6 justify-end'>
-        <Button variant='outline'>저장</Button>
-        <Button onClick={handleSubmit}>발행</Button>
+        {/* <Button variant='outline'>저장</Button> */}
+        {isSetting && <Button variant='outline' onClick={() => setIsSetting(false)}>취소</Button>}
+        <Button onClick={isSetting ? handleSubmit : () => setIsSetting(true)}>발행</Button>
+        <Dialog>
+          <DialogTrigger>Open</DialogTrigger>
+          <DialogContent>
+            <DialogHeader>
+              <DialogTitle>Are you absolutely sure?</DialogTitle>
+              <DialogDescription>
+                This action cannot be undone. This will permanently delete your account
+                and remove your data from our servers.
+              </DialogDescription>
+            </DialogHeader>
+          </DialogContent>
+        </Dialog>
       </div>
+
+
     </>
 
   );
