eb2332d306db3c54e55ff1950d7725a2f3db96ef
jhlee <wisdom_13_@naver.com>
2024-07-31 02:30:06 +0900

    🔨 권한 체크 hook 생성

---
+++ hooks/useUserHouseRole.ts
@@ -0,0 +1,45 @@
+import { useEffect, useState } from 'react';
+import { Family, UserDetails } from '@/types';
+import { useUser } from './useUser';
+import { useHouseBuildByAddress } from './useHouseBuilder';
+
+interface UserHouseRole {
+  role?: string;
+  isFamily: boolean;
+  isEditer: boolean;
+  isOwner: boolean;
+  profiles?: UserDetails[];
+}
+
+export const useUserHouseRole = (address: string): UserHouseRole => {
+  const { user, userDetails } = useUser();
+  const { data } = useHouseBuildByAddress(address);
+  const [role, setRole] = useState<UserHouseRole>({ isFamily: false, isEditer: false, isOwner: false });
+
+  useEffect(() => {
+    if (user && userDetails && data) {
+      const houseFamily = data.family;
+      const familyUser = houseFamily.find(member => member.user_id === user.id);
+
+      if (familyUser) {
+        const familyProfile = {
+          id: user.id,
+          name: familyUser?.nick_name,
+          avatar_url: familyUser?.avatar_path,
+        }
+
+        setRole({
+          role: familyUser.role,
+          isFamily: true,
+          isEditer: true,
+          isOwner: familyUser.is_owner,
+          profiles: [userDetails, familyProfile]
+        });
+      } else {
+        setRole({ isFamily: false, isEditer: false, isOwner: false });
+      }
+    }
+  }, [user, userDetails, data]);
+
+  return role;
+};
